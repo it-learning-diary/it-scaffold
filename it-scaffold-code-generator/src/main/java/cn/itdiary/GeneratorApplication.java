5138badbd2de2ef701ad0a28abba0f2498b78d01
it-learning-diary <2160305040@qq.com>
2023-01-17 18:09:53 +0800

    集成项目基础依赖+日志框架

---
+++ it-scaffold-code-generator/src/main/java/cn/itdiary/GeneratorApplication.java
@@ -0,0 +1,19 @@
+package cn.itdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 代码生成器启动类
+ * @Author: it-diary-learning
+ * @Version: v1
+ * @Date: 2023/1/17:17:02
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class,args);
+    }
+
+}
